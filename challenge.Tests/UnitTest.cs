2653816bd460e794ad5ed3cf33c1d54cc3528b62
ying <sutimar@mycostech.com>
2025-04-19 15:53:33 +0700

    first commit

---
+++ challenge.Tests/UnitTest.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using challenge;
+
+namespace challenge.Tests;
+
+public class UnitTest
+{
+    [Fact]
+    [Description("Input 222 2 22# should be show CAB")]
+    public void Input_ShouldShowCAB()
+    {
+        var result = new Phone().OldPhonePad("222 2 22#");
+        Assert.Equal("CAB", result);
+    }
+
+    [Fact]
+    [Description("Input 2222 2 22# should be show AAB")]
+    public void Input_ShouldShowAAB()
+    {
+        var result = new Phone().OldPhonePad("2222 2 22#");
+        Assert.Equal("AAB", result);
+    }
+
+    [Fact]
+    [Description("Input 4433555 555666# should be show HELLO")]
+    public void Input_ShouldShowHello()
+    {
+        var result = new Phone().OldPhonePad("4433555 555666#");
+        Assert.Equal("HELLO", result);
+    }
+
+    [Fact]
+    [Description("Input 227*# should be show B")]
+    public void Input_ShouldShowB()
+    {
+        var result = new Phone().OldPhonePad("227*#");
+        Assert.Equal("B", result);
+    }
+
+    [Fact]
+    [Description("Input 8 88777444666*664# should be show TURING")]
+    public void Input_ShouldShowTuring()
+    {
+        var result = new Phone().OldPhonePad("8 88777444666*664#");
+        Assert.Equal("TURING", result);
+    }
+}
---
+++ challenge/Phone.cs
@@ -0,0 +1,52 @@
+namespace challenge;
+
+public class Phone
+{
+    private Dictionary<char, char[]> characterList = new Dictionary<char, char[]>()
+        {
+            {'2', [ 'A', 'B', 'C' ]},
+            {'3', [ 'D', 'E', 'F' ]},
+            {'4', [ 'G', 'H', 'I' ]},
+            {'5', [ 'J', 'K', 'L' ]},
+            {'6', [ 'M', 'N', 'O' ]},
+            {'7', [ 'P', 'Q', 'R', 'S']},
+            {'8', [ 'T', 'U', 'V']},
+            {'9', [ 'W', 'X', 'Y', 'Z']},
+            };
+
+    public string OldPhonePad(string input)
+    {
+        // process
+        string translateText = "";
+        char[] specialCharacter = [' ', '*', '#'];
+
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (i == 0)
+                continue;
+
+            if (input[i] == input[end])
+            {
+                end = i;
+            }
+            else
+            {
+                if (!specialCharacter.Contains(input[end]))
+                {
+                    var _allCharacter = characterList[input[end]];
+                    translateText += _allCharacter[(end - start) % _allCharacter.Length];
+                }
+                if (input[i] == '*')
+                {
+                    translateText = translateText.Remove(translateText.Length - 1);
+                }
+                start = i;
+                end = i;
+            }
+        }
+
+        return translateText;
+    }
+};
